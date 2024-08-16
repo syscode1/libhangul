7db87f8974fd3091765e4e0e1fc725dbc47d9b57
User <syscode.dev@outlook.com>
2024-08-16 15:04:18 +0900

    add build.bat batch build

---
+++ build.bat
@@ -0,0 +1,7 @@
+cmake --preset vs2022-x64
+cmake --build --preset vs2022-x64-debug
+cmake --build --preset vs2022-x64-release
+
+cmake --preset vs2022-x86
+cmake --build --preset vs2022-x86-debug
+cmake --build --preset vs2022-x86-release
